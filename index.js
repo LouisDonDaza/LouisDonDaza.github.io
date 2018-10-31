8b4970f3f1dd6e7e2252706979f38a97cb3948d9
Louis Daza <louisdaza@hotmail.com>
2018-10-31 16:41:03 -0700

    small changes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import Primary from './Primary';
 import 'tachyons';
 
 const domContainer = document.querySelector('#indexSite');
-ReactDOM.render(<Primary/>, domContainer);
+ReactDOM.render(e(Primary), domContainer);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
