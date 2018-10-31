b70667b050e571ace0e3ee023ec0d224ef7835cc
Louis Daza <louisdaza@hotmail.com>
2018-10-31 16:47:02 -0700

    added source code

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import Primary from './Primary';
 import 'tachyons';
-
-const domContainer = document.querySelector('#indexSite');
-ReactDOM.render(e(Primary), domContainer);
+ReactDOM.render(<Primary/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
